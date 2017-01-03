901837cc6a248cd52c8994e679afc7c9385f1590
Jonah Joselow <jonahjoselow@gmail.com>
2017-01-02 22:56:42 -0500

    rename

---
+++ arduino-game.ino
@@ -0,0 +1,27 @@
+#ifndef DELAY
+    #define DELAY 100
+#endif /* DELAY */
+
+#include "Conway.h"
+
+//Conway game_of_life = Conway(8, 8, 1, 1, 6);
+Conway game_of_life = Conway(8, 8, 2, 2, 6);
+
+void setup() {
+
+    #if (CONWAY_DEBUG)
+        Serial.begin(9600);
+    #endif /* CONWAY_DEBUG */
+
+    game_of_life.init();
+}
+
+void loop() {
+    game_of_life.draw();
+
+    #if (DELAY > 0)
+        delay(DELAY);
+    #endif /* DELAY */
+
+    game_of_life.update();
+}
